890d58277a73c00ce946bc2a5257599e996758e2
jun-hwan00 <0528klkl@naver.com>
2025-12-19 18:34:03 +0900

    feat: django 서버 파일 변경

--- PhotoBlogServer/blog/views.py
+++ PhotoBlogServer/blog/views.py
@@ -1,12 +1,35 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer
-from django.shortcuts import render
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer  # ← 줄바꿈!
+
 class BlogImages(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
+    def perform_create(self, serializer):
+        # 게시물 저장
+        post = serializer.save()
+        
+        # WebSocket으로 알림 전송
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            'notifications',
+            {
+                'type': 'detection_alert',
+                'message': {
+                    'type': 'detection_alert',
+                    'object_name': post.title or 'Unknown',
+                    'confidence': 0.85,
+                    'timestamp': post.created_date.isoformat(),
+                    'image_id': post.id,
+                    'image_url': post.image.url if post.image else None
+                }
+            }
+        )
+
 def post_list(request):
-    """블로그 형태로 게시물 목록 표시"""
-    posts = Post.objects.all().order_by('-published_date')
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
--- PhotoBlogServer/mysite/settings.py
+++ PhotoBlogServer/mysite/settings.py
@@ -40,11 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',          
+    'corsheaders',
     'rest_framework.authtoken', 
+      'channels',
     'blog',  
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,7 +76,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+CORS_ALLOW_CREDENTIALS = True
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
